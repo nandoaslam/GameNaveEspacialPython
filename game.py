23e2d62eeaa8ec7785c1283105f8bbbc934c7932
Sisnando Junior <bugdroidgamesbr@gmail.com>
2017-04-04 17:16:12 -0300

    Add files via upload

--- game.py
+++ game.py
@@ -1,92 +1,27 @@
-import pygame, sys
+import pygame
 from pygame.locals import *
-# GAME: INVASÃO ESPACIAL
-#AUTOR: SISNANDO JUNIOR
+from sys import exit
 
-largura = 900
-altura = 400
+pygame.init()
 
-class Bala(pygame.sprite.Sprite):
-    def __init__(self, posx, posy):
-        pygame.sprite.Sprite.__init__(self)
-        self.ImagemBala = pygame.image.load('imagens/baladourada.png')
+screen = pygame.display.set_mode((900, 400), 0, 32)
 
-        self.rect = self.ImagemBala.get_rect()
-        self.velocidadeBala = 5
-        self.rect.top = posy
-        self.rect.left = posx
+background_file = 'imagens/fundo.jpg'
+background = pygame.image.load(background_file).convert()
+jogando = True
 
-    def trajetoria (self):
-        self.rect.top = self.rect.top - self.velocidadeBala
+ship_filename = 'imagens/nave.png'
+ship = pygame.image.load(ship_filename).convert_alpha()
 
-    def colocar (self, superficie):
-        superficie.blit(self.ImagemBala, self.rect)
+pygame.display.set_caption('Nave Espacial')
 
-class naveEspacial(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.ImagemNave = pygame.image.load("imagens/nave.png")
+clock = pygame.time.Clock()
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
 
-        self.rect = self.ImagemNave.get_rect()
-        self.rect.centerx = largura / 2
-        self.rect.centery = altura - 60
-
-        self.listaDisparo = []
-        self.vida = True
-        self.velocidade = 20
-
-#CLASSE PARA DEFINIR O LIMITE DO MOVIMENTO DO JOGADOR
-    def movimento(self):
-        if self.vida == True:
-            if self.rect.left <= 0:
-                self.rect.left = 0
-
-            elif self.rect.right > 900:
-                self.rect.right = 900
-
-    def disparar(self):
-        print('Disparou')
-
-    def colocar(self, superficie):
-        superficie.blit(self.ImagemBala, self.rect)
-    
-#DEFININDO RESOLUÇÃO DA TELA E NOME DO JOGO
-    def invasaoEspaco():
-        pygame.init()
-        tela = pygame.display.set_mode((largura, altura))
-        pygame.display.set_caption("Invasão do Espaço")
-
-#ADICIONANDO IMAGEM DE FUNDO
-        jogador = naveEspacial()
-        ImagemFundo = pygame.image.load('imagens/fundo.jpg')
-        jogando = True
-
-    balaProjetil = Bala(largura / 2, altura - 60)
-
-#COMANDOS DE MOVIMENTOS
-    while True:
-        jogador.movimento()
-        balaProjetil.trajetoria()
-        for evento in pygame.event.get():
-            if evento.type == QUIT:
-                pygame.quit()
-                sys.exit()
-
-            if evento.type == pygame.KEYDOWN:
-                    if evento.key == K_LEFT:
-                        jogador.rect.left -= jogador.velocidade
-
-                    elif evento.key == K_RIGHT:
-                        jogador.rect.right += jogador.velocidade
-
-                    elif evento.key == K_SPACE:
-                        jogador.disparar()
-
-            tela.blit(ImagemFundo,(0,0))
-            balaProjetil.colocar(tela)
-            jogador.colocar(tela)
-            pygame.display.update()
-
-invasaoEspaco()
-
-
+    screen.blit(background, (0, 0))
+    screen.blit(ship, (400,300))
+    pygame.display.update()
+    time_passed = clock.tick(30)
